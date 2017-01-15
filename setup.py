e0957bc4a50213983ea12f083eb7cc9e78ee2abc
Priyam Singh <priyamsingh.22296@gmail.com>
2017-01-15 16:00:32 +0530

    Prep for 0.1.0 release

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from setuptools import setup
 
 setup(name='beautifultable',
-      version='0.1',
+      version='0.1.0',
       description='Utility package to print visually appealing ASCII tables to terminal',
       author='Priyam Singh',
       author_email='priyamsingh.22296@gmail.com',
       packages=['beautifultable'],
-      zip_safe=False)
+      url='https://github.com/pri22296/beautifultable',
+      keywords='table terminal ascii',
+      license='MIT',
+      install_requires=[
+          'enum34;python_version<"3.4"',
+      ]
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Printing',
+          'Topic :: Text Processing',
+      ])
