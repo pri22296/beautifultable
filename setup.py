80b373a3dc3967dba84a3e44e453e6dbfa83133c
Priyam Singh <priyamsingh.22296@gmail.com>
2017-01-17 17:18:06 +0530

    prep for 0.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='beautifultable',
-      version='0.1.0',
+      version='0.1.1',
       description='Utility package to print visually appealing ASCII tables to terminal',
       author='Priyam Singh',
       author_email='priyamsingh.22296@gmail.com',
