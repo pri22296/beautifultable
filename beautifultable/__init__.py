a5b3f2bc10b6c57148d82219c87be3be4280509e
Priyam Singh <priyamsingh.22296@gmail.com>
2018-06-30 11:45:04 +0530

    Update version to 0.5.2 for next release

--- beautifultable/__init__.py
+++ beautifultable/__init__.py
@@ -1,4 +1,5 @@
 from .beautifultable import BeautifulTable
 
+__version__ = '0.5.2'
 __all__ = ['BeautifulTable']
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import sys
 
-version = '0.5.1'
+version = '0.5.2'
 extras_require = {':python_version<"3.4"': ['enum34']}
 
 setup(name='beautifultable',
